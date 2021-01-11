c56917a81a10aff6a79dccf1092daea9fa4de3fb
Mathéo Grail <grail.matheo@protonmail.com>
2021-01-11 10:22:19 +0100

    Minifier
    - Réduction de la taille des fichiers JS et CSS.
    - La page "Nous contacter" s'affiche correctement : les différents fichiers sont désormais appelés correctement.

--- assets/includes/form.php
+++ assets/includes/form.php
@@ -9,16 +9,14 @@
 	$input_504 = $_POST['input_504'];
 	$message = $_POST['message'];
 	
-	$to = 'receiver@yoursite.com'; // Email submissions are sent to this email
+	$to = 'receiver@yoursite.com';
 
-	// Create email	
 	$email_subject = "Message from your website";
-	$email_body = "You have received a new message. \n\n".
-				  "Name: $name \nEmail: $email \nInput_504: $input_504 \nMessage: $message \n";
+	$email_body = "You have received a new message. \n\n"."Name: $name \nEmail: $email \nInput_504: $input_504 \nMessage: $message \n";
 	$headers = "MIME-Version: 1.0\r\nContent-type: text/plain; charset=UTF-8\r\n";	
 	$headers .= "From: contact@yoursite.com\n";
 	$headers .= "Reply-To: $input_504";	
 	
-	mail($to,$email_subject,$email_body,$headers); // Post message
+	mail($to,$email_subject,$email_body,$headers);
 	return true;			
 ?>
